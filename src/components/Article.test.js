1a7a37d5709a63bbc25b4dee88a06e27f5496c1c
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-11-12 17:42:47 -0500

    2

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
-import {render} from 'react-dom'
+import {render} from '@testing-library/react'
 
 const testArticle = {
     id: 'aMqwd', //unique article id
@@ -15,18 +15,18 @@ const testArticle = {
       body: ""  //paragraph of article text
 }
 
-
 test('renders component without errors', ()=> {
+    render(<Article article={testArticle}/>)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+});
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+test('renders "Associated Press" when no author is given', ()=> {
+});
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+test('executes handleDelete when the delete button is pressed', ()=> {
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
