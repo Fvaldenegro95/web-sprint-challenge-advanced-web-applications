10d449d5992a2a843a0480bbb53f2cbdb4d02295
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-11-12 18:00:14 -0500

    articletestcomplete

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
-import {render, screen } from '@testing-library/react'
+import {render, screen, waitFor } from '@testing-library/react'
 
 const testArticle = {
     id: 'aMqwd', //unique article id
@@ -48,9 +48,19 @@ test('renders "Associated Press" when no author is given', ()=> {
     expect(author).toBeInTheDocument()
 });
 
-test('executes handleDelete when the delete button is pressed', ()=> {
-    render(<Article article={testArticle}/>)
-});
+test('executes handleDelete when the delete button is pressed', async ()=> {
+    const displayFunc = jest.fn()
+    
+    render(<Article article={testArticle} handleDelete={displayFunc}/>)
 
+    
+    const button = screen.getByTestId("deleteButton")
+    userEvent.click(button)
+
+    await waitFor( () => {
+        expect(displayFunc).toHaveBeenCalled()
+    })
+
+});
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
